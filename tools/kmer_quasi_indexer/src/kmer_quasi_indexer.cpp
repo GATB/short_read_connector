e62dd648eb537d8a72553357ad292b7cf6cc914a
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2016-05-03 15:33:05 +0200

    bug fix -- important

--- tools/kmer_quasi_indexer/src/kmer_quasi_indexer.cpp
+++ tools/kmer_quasi_indexer/src/kmer_quasi_indexer.cpp
@@ -60,42 +60,17 @@ void kmer_quasi_indexer::create_quasi_dictionary (int fingerprint_size){
 
 
 
-	double lg2 = log(2);
-//	float NBITS_PER_KMER = log (16*kmer_size*(lg2*lg2))/(lg2*lg2);
-//	NBITS_PER_KMER = 12;
 	/** We get the number of solid kmers. */
-	const u_int64_t solidFileSize = solidKmers.getNbItems();
-	//	u_int64_t estimatedBloomSize = (u_int64_t) ((double)solidFileSize * NBITS_PER_KMER);
-	//	if (estimatedBloomSize ==0 ) { estimatedBloomSize = 1000; }
-	//	int nb_hash				= 7;
-
-
 	nbSolidKmers = solidKmers.getNbItems();
+	if(nbSolidKmers==0){
+		cout<<"No solid kmers in bank -- exit"<<endl;
+		exit(0);
+	}
 
 
 
 	IteratorKmerH5Wrapper iteratorOnKmers (solidKmers.iterator());
 
-	//	int i=0;
-	//    cout << "---------------------------------------------------- " << endl;
-	//
-	//
-	//	for(auto  &element: iteratorOnKmers){
-	//		cout<<"first  "<<element<<endl;
-	//	}
-	//
-	//    cout << "---------------------------------------------------- " << endl;
-	//	i=0;
-	//	for(auto  &element : iteratorOnKmers){
-	//		i++;
-	//		if (i<10) {cout<<"next 10: "<<element<<endl;}
-	//		else break;
-	//	}
-	//    cout << "---------------------------------------------------- " << endl;
-
-
-
-	//	quasiDico = quasiDictionnary<IteratorKmerH5, std::vector< list<u_int32_t> > (nbSolidKmers, iteratorOnKmers, all_list.begin(), fingerprint_size, sizeof(list<u_int32_t>));
 	quasiDico = quasiDictionnaryKeyGeneric<IteratorKmerH5Wrapper, u_int32_t> (nbSolidKmers, iteratorOnKmers, fingerprint_size, 2); // gamma = 2
 
 
@@ -113,31 +88,31 @@ static int NT2int(char nt)  {  return (nt>>1)&3;  }
  *********************************************************************/
 inline bool highComplexity(Sequence& seq){
 	const char* data = seq.getDataBuffer();
-    int DUSTSCORE[64]; // all tri-nucleotides
-    for (int i=0; i<64; i++) DUSTSCORE[i]=0;
-    size_t lenseq =seq.getDataSize();
+	int DUSTSCORE[64]; // all tri-nucleotides
+	for (int i=0; i<64; i++) DUSTSCORE[i]=0;
+	size_t lenseq =seq.getDataSize();
 
-    for (int j=2; j<lenseq; ++j) ++DUSTSCORE[NT2int(data[j-2])*16 + NT2int(data[j-1])*4 + NT2int(data[j])];
-    int m,s=0;
+	for (int j=2; j<lenseq; ++j) ++DUSTSCORE[NT2int(data[j-2])*16 + NT2int(data[j-1])*4 + NT2int(data[j])];
+	int m,s=0;
 
-    for (int i=0; i<64; ++i)
-    {
-        m = DUSTSCORE[i];
-        s  += (m*(m-1))/2;
-    }
+	for (int i=0; i<64; ++i)
+	{
+		m = DUSTSCORE[i];
+		s  += (m*(m-1))/2;
+	}
 
-    return s<((lenseq-2)/4 * (lenseq-6)/4)/2;
+	return s<((lenseq-2)/4 * (lenseq-6)/4)/2;
 }
 
 inline const bool correctSequence(Sequence& s){
 
 	const char* data = s.getDataBuffer();
 
-    for (size_t i=0; i<s.getDataSize(); i++)
-    {
-        if (data[i]!='A' && data[i]!='C' && data[i]!='G' && data[i]!='T')  { return false; }
-    }
-    return true;
+	for (size_t i=0; i<s.getDataSize(); i++)
+	{
+		if (data[i]!='A' && data[i]!='C' && data[i]!='G' && data[i]!='T')  { return false; }
+	}
+	return true;
 }
 
 // We define a functor that will be cloned by the dispatcher
@@ -170,9 +145,12 @@ struct FunctorIndexer
 			// Adding the read id to the list of ids associated to this kmer.
 			// note that the kmer may not exist in the dictionnary if it was under the solidity threshold.
 			// in this case, nothing is done
-//			synchro->lock();
+			//			synchro->lock();
+
 			quasiDico.set_value(itKmer->value().getVal(), read_id, synchro);
-//			synchro->unlock();
+//
+			cout<<model.toString(itKmer->value().getVal())<<" read id "<<read_id<<endl;
+			//			synchro->unlock();
 		}
 	}
 };
@@ -202,24 +180,7 @@ void kmer_quasi_indexer::fill_quasi_dictionary (const int nbCores){
 
 
 	delete synchro;
-	//
-	//    // We loop over sequences.
-	//    for (itSeq.first(); !itSeq.isDone(); itSeq.next())
-	//    {
-	//        // We set the data from which we want to extract kmers.
-	//        itKmer.setData (itSeq->getData());
-	//        if (containsN(itSeq->getDataBuffer())){continue;}
-	////        cout<<itSeq->getDataBuffer();
-	//        // We iterate the kmers.
-	//        for (itKmer.first(); !itKmer.isDone(); itKmer.next())
-	//        {
-	//        	// Adding the read id to the list of ids associated to this kmer.
-	//        	// note that the kmer may not exist in the dictionnary if it was under the solidity threshold.
-	//        	// in this case, nothing is done
-	//        	u_int32_t truc = static_cast<u_int32_t>(itSeq->getIndex());
-	//            quasiDico.set_value(itKmer->value().getVal(), truc);
-	//        }
-	//    }
+
 }
 
 
@@ -243,23 +204,23 @@ struct FunctorQuery
 	}
 
 	~FunctorQuery () { // TODO: put back these "delete". Currently (3 may 2016) they create a segfault.
-//		cout<<"HEY CEST FINI"<<endl;
-//			delete model; //= new Kmer<KMER_SPAN(0)>::ModelCanonical (kmer_size);
-//			delete itKmer; // = new Kmer<KMER_SPAN(0)>::ModelCanonical::Iterator (*model);
-		}
+		//		cout<<"HEY CEST FINI"<<endl;
+		//			delete model; //= new Kmer<KMER_SPAN(0)>::ModelCanonical (kmer_size);
+		//			delete itKmer; // = new Kmer<KMER_SPAN(0)>::ModelCanonical::Iterator (*model);
+	}
 	void operator() (Sequence& seq)
 	{
 
 		if (!correctSequence(seq)){// || !highComplexity(seq)){
-			synchro->lock ();
-			outFile<<seq.getIndex()<<" U"<<endl;
-			synchro->unlock ();
+			//			synchro->lock ();
+			//			outFile<<seq.getIndex()<<" U"<<endl;
+			//			synchro->unlock ();
 
 			return;}
 
 		bool exists;
 		vector<u_int32_t> associated_read_ids;
-//		vector<u_int32_t> similar_read_ids;
+		//		vector<u_int32_t> similar_read_ids;
 
 
 
@@ -273,34 +234,34 @@ struct FunctorQuery
 		for (itKmer->first(); !itKmer->isDone(); itKmer->next())
 		{
 			quasiDico.get_value((*itKmer)->value().getVal(),exists,associated_read_ids);
-
+			if(!exists) continue;
 			for(auto &read_id: associated_read_ids){
 				std::unordered_map<u_int32_t, std::pair <u_int,u_int>>::const_iterator element = similar_read_ids_position_count.find(read_id);
 				if(element == similar_read_ids_position_count.end()) {// not inserted yet:
 					similar_read_ids_position_count[read_id]=std::make_pair(i+kmer_size, 1);
-//					cout<<"create a new pair for "<<read_id<<" ("<<(i+kmer_size)<< ", 1)"<<endl;
+//										cout<<"create a new pair for "<<read_id<<" ("<<(i+kmer_size)<< ", 1)"<<endl;
 				}
 				else{  // a kmer is already shared with this read
 					std::pair <int,int> viablepos_nbshared = (element->second);
-//					cout<<"adding position "<<i<<" for kmer "<<read_id<<" ?"<<endl;
-//					cout<<i<<" >=? "<<viablepos_nbshared.first<<endl;
+//										cout<<"adding position "<<i<<" for kmer "<<read_id<<" ?"<<endl;
+					//					cout<<i<<" >=? "<<viablepos_nbshared.first<<endl;
 					if(i>=viablepos_nbshared.first){ // the current position does not overlap the previous shared kmer
-//						cout<<"YES"<<endl;
+						//						cout<<"YES"<<endl;
 						viablepos_nbshared.first = i+kmer_size; // next non overlapping position
 						viablepos_nbshared.second = viablepos_nbshared.second+1; // a new kmer shared.
 						similar_read_ids_position_count[read_id] = viablepos_nbshared;
 					}
-//					else cout<<"NO"<<endl;
-//					cout<<"cupdated pair for "<<read_id<<" ("<<viablepos_nbshared.first<<", "<<viablepos_nbshared.second<< ")"<<endl;
+					//					else cout<<"NO"<<endl;
+					//					cout<<"cupdated pair for "<<read_id<<" ("<<viablepos_nbshared.first<<", "<<viablepos_nbshared.second<< ")"<<endl;
 
 				}
 			}
 
-//			similar_read_ids.insert(similar_read_ids.end(),associated_read_ids.begin(),associated_read_ids.end());
+			//			similar_read_ids.insert(similar_read_ids.end(),associated_read_ids.begin(),associated_read_ids.end());
 			i++;
 
 		}
-//		sort(similar_read_ids.begin(),similar_read_ids.end());
+		//		sort(similar_read_ids.begin(),similar_read_ids.end());
 
 		// We lock the synchronizer
 
@@ -350,50 +311,11 @@ void kmer_quasi_indexer::parse_query_sequences (int threshold, const int nbCores
 	ISynchronizer* synchro = System::thread().newSynchronizer();
 
 	// We create a dispatcher configured for 'nbCores' cores.
-	Dispatcher dispatcher (nbCores, 1000);
+	Dispatcher dispatcher (1, 1000);
 	// We iterate the range.  NOTE: we could also use lambda expression (easing the code readability)
 	dispatcher.iterate (itSeq, FunctorQuery(synchro,outFile, kmer_size, quasiDico, threshold));
 
 
-
-	// We loop over sequences.
-
-	//    for (itSeq.first(); !itSeq.isDone(); itSeq.next())
-	//    {
-	//
-	//        if (containsN(itSeq->getDataBuffer())){read_id++; continue;}
-	//  	  vector<u_int32_t>similar_read_ids;
-	//
-	//
-	//
-	//        // We set the data from which we want to extract kmers.
-	//        itKmer.setData (itSeq->getData());
-	//        // We iterate the kmers.
-	//        for (itKmer.first(); !itKmer.isDone(); itKmer.next())
-	//        {
-	//        	quasiDico.get_value(itKmer->value().getVal(),exists,associated_read_ids);
-	//        	similar_read_ids.insert(similar_read_ids.end(),associated_read_ids.begin(),associated_read_ids.end());
-	//
-	//        }
-	//        sort(similar_read_ids.begin(),similar_read_ids.end());
-	//        outFile<<read_id<<" ";
-	//        u_int32_t prev_id=-1;
-	//        int nb_shared=0;
-	//        for (auto &element:similar_read_ids){
-	//        	if(prev_id != element){
-	//        		if(prev_id!=-1){
-	//        			if(nb_shared>threshold)
-	//        				outFile<<"["<<prev_id<<" "<<nb_shared<<"] ";
-	//        		}
-	//        		nb_shared=0;
-	//        		prev_id=element;
-	//        	}
-	//        	nb_shared++;
-	//        }
-	//        outFile<<endl;
-	//
-	//        read_id++;
-	//    }
 
 
 	outFile.close();    // We get rid of the synchronizer
@@ -421,11 +343,11 @@ void kmer_quasi_indexer::execute ()
 
 
 	// IMPORTANT NOTE:
-		// Actually, during the filling of the dictionary values, one may fall on non solid non indexed kmers
-		// that are quasi dictionary false positives (ven with a non null fingerprint. This means that one nevers knows in advance how much
-		// values are gonna be stored for all kmers. This is why I currently us a vector<u_int32_t> for storing read ids associated to a kmer.
+	// Actually, during the filling of the dictionary values, one may fall on non solid non indexed kmers
+	// that are quasi dictionary false positives (ven with a non null fingerprint. This means that one nevers knows in advance how much
+	// values are gonna be stored for all kmers. This is why I currently us a vector<u_int32_t> for storing read ids associated to a kmer.
 
-		// We need a non null finger print because of non solid non indexed kmers
+	// We need a non null finger print because of non solid non indexed kmers
 	//	if (getInput()->getStr(STR_URI_BANK_INPUT).compare(getInput()->getStr(STR_URI_QUERY_INPUT))==0)
 	//		fingerprint_size=0;
 	cout<<"fingerprint = "<<fingerprint_size<<endl;
