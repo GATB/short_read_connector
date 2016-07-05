d9d95f2562c583519dea9258e91cd95b7f721802
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2016-07-05 16:00:12 +0200

    updating scripts wrt blast comparisons

--- scripts/filter_blast_results.py
+++ scripts/filter_blast_results.py
@@ -14,17 +14,34 @@ def filter(file_name, threshold):
     #OM-RGC.v1.000000024_Eukaryota	OM-RGC.v1.008747545_Eukaryota	2.22e-04	96.970	33	30690	825
     previous_query="rien"
     previous_target="rien"
+    previous_ratio=0
     for line in sequencefile.readlines():
         tab_line = line.rstrip().split()
-        if tab_line[0]==previous_query and tab_line[1]==previous_target: continue
-        previous_query=tab_line[0]
-        previous_target=tab_line[1]
+            
         # the total size of aligned sequence aligned=(float(tab_line[3])*float(tab_line[4]))
         aligned=(float(tab_line[3])*float(tab_line[4]))
         # divided by
         ratio = aligned/float(tab_line[5])
-        if  ratio <threshold: continue
-        print tab_line[0]+'\t'+tab_line[1]+'\t'+tab_line[2]+'\t'+str(ratio)
+
+        # this couple had been seen already: we update the ratio if necessary
+        if tab_line[0]==previous_query and tab_line[1]==previous_target:
+            if ratio>previous_ratio: previous_ratio=ratio
+            continue
+        # this is a new couple, we print the previous one
+        else: 
+            if previous_ratio>=threshold:
+                if previous_query!="rien":
+                    print previous_query+'\t'+previous_target+'\t0\t'+str(previous_ratio)
+        
+        # store the current values
+        previous_query=tab_line[0]
+        previous_target=tab_line[1]
+        previous_ratio=ratio
+        
+    # last line to print
+    if previous_ratio>=threshold:
+        if previous_query!="rien":
+            print previous_query+'\t'+previous_target+'\t0\t'+str(previous_ratio)
     
     
 if len(sys.argv)<3 :
--- scripts/remove_auto_links.py
+++ scripts/remove_auto_links.py
@@ -12,6 +12,7 @@ def remove_auto_linked_sequences(file_name):
     #read18_contig0_position4295_M0_I0_D0_NG0______er0.01__indel0__rgeom0    read687_contig0_position4286_M1_I0_D0_NG0______er0.01__indel0__rgeom0   0.0     74.26
     
     for line in sequencefile.readlines():
+        if line[0]=='#': continue
         tab_line = line.rstrip().split()
         if tab_line[0]==tab_line[1]: continue
         print line,
