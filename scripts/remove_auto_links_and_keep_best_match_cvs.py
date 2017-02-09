628e9f8185fce80a2cba7b88cc243b00c123dcee
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2017-02-09 14:19:25 +0100

    script for blast layout

--- scripts/remove_auto_links_and_keep_best_match_cvs.py
+++ scripts/remove_auto_links_and_keep_best_match_cvs.py
@@ -10,12 +10,15 @@ def remove_auto_linked_sequences(file_name):
         sequencefile=open(file_name,"r")
         
     #read18_contig0_position4295_M0_I0_D0_NG0______er0.01__indel0__rgeom0    read687_contig0_position4286_M1_I0_D0_NG0______er0.01__indel0__rgeom0   0.0     74.26
-    
+    prev=""
     for line in sequencefile.readlines():
         if line[0]=='#': continue
         tab_line = line.rstrip().split()
         if tab_line[0]==tab_line[1]: continue
+        cur=tab_line[0]+tab_line[1]
+        if cur==prev: continue
         print line,
+        prev=cur
     
     
 if len(sys.argv)<2 :
